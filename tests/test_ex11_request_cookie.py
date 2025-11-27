52d8cbd3cc887a8ab1b9697b3d8ec8a13ac3803d
Valiev Robert <valievrob@mail.ru>
2025-11-27 23:08:57 +0500

    Ex11: Тест запроса на метод cookie

--- tests/test_ex11_request_cookie.py
+++ tests/test_ex11_request_cookie.py
@@ -5,4 +5,11 @@ import json
 class TestRequestCookie:
     def test_request_cookie(self):
         response = requests.get("https://playground.learnqa.ru/api/homework_cookie")
-        print(response.cookies)
+        print(response.cookies.get_dict())
+        print(response.cookies.get("HomeWork"))
+        expected_cookie_name = "HomeWork"
+        expected_cookie_value = "hw_value"
+
+        assert expected_cookie_name in response.cookies, f"Не удалось найти куки с именем {expected_cookie_name} в ответе"
+
+        assert expected_cookie_value == response.cookies.get(expected_cookie_name), f"Фактическое значение cookie отличается от ожидаемого"
